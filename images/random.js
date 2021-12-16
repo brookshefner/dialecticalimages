1d1d50a7c88a524132cdae302bf7bdca40a5f634
brookshefner <36647441+brookshefner@users.noreply.github.com>
2021-12-16 10:26:59 -0500

    Create random.js

---
+++ images/random.js
@@ -0,0 +1,17 @@
+<!--
+function random_text()
+{};
+var random_text = new random_text();
+// Set the number of text strings to zero to start
+var number = 0;
+// Incremental list of all possible Text
+random_text[number++] = "<img src=v/vspfiles/templates/100/images2/random1.jpg width=591 height=202>"
+random_text[number++] = "<img src=v/vspfiles/templates/100/images2/random2.jpg width=591 height=202>"
+random_text[number++] = "<img src=v/vspfiles/templates/100/images2/random3.jpg width=591 height=202>"
+random_text[number++] = "<img src=v/vspfiles/templates/100/images2/random4.jpg width=591 height=202>"
+// Create a random number with limits based on the number
+// of possible random text strings
+var random_number = Math.floor(Math.random() * number);
+// Write out the random text to the browser
+document.write(random_text[random_number]);
+-->
